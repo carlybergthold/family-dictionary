2c08c63e18668ae5311f151b7a9d0dbcac542087
Carly Bergthold <carlybergthold@gmail.com>
2019-07-09 10:08:55 -0500

    second commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace family_dictionary
 {
@@ -6,7 +7,36 @@ namespace family_dictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+        // Define a Dictionary that contains information about several members of your family.
+        var myFamily = new Dictionary<string, Dictionary<string, string>>();
+        myFamily.Add("dad", new Dictionary<string, string>(){
+            {"name", "Ron"},
+            {"age", "59"}
+        });
+        myFamily.Add("mom", new Dictionary<string, string>(){
+            {"name", "Gwyn"},
+            {"age", "58"}
+        });
+        myFamily.Add("oldest sister", new Dictionary<string, string>(){
+            {"name", "Cara"},
+            {"age", "34"}
+        });
+        myFamily.Add("sister", new Dictionary<string, string>(){
+            {"name", "Cali"},
+            {"age", "31"}
+        });
+        myFamily.Add("brother", new Dictionary<string, string>(){
+            {"name", "Caleb"},
+            {"age", "23"}
+        });
+        // Next, iterate over each item in myFamily and produce the following output. Remember that you can use square bracket notation to get to the value of a key, and that a dictionary has a Key and a Value property.
+        foreach(KeyValuePair<string, Dictionary<string, string>> familyMember in myFamily)
+        {
+            string name = familyMember.Value["name"];
+            string age = familyMember.Value["age"];
+            Console.WriteLine($"{name} is my {familyMember.Key} and is {age} years old.");
+        }
+        // Krista is my sister and is 42 years old
         }
     }
 }
